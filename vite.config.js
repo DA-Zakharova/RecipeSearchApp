21fc8b2335bf67b9c9b68f07cfa89de818d52268
DA-Zakharova <dashazakharova0301@gmail.com>
2025-12-25 13:54:49 +0300

    Configure GitHub Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-    server: {
+  base: "/RecipeSearchApp/",
+  server: {
     open: true,
     port: 3012,
   }
